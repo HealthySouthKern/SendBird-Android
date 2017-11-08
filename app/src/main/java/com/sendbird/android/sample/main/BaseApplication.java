a3ccf3d11aed07c2c5edbae7d479b549b0b1a922
Eddie Rangel <hacker@eddierangel.com>
2017-11-07 16:06:52 -0800

    Update APP ID
    US-1 Demo -> SouthKernTest01

--- app/src/main/java/com/sendbird/android/sample/main/BaseApplication.java
+++ app/src/main/java/com/sendbird/android/sample/main/BaseApplication.java
@@ -7,7 +7,7 @@ import com.sendbird.android.SendBird;
 
 public class BaseApplication extends Application {
 
-    private static final String APP_ID = "9DA1B1F4-0BE6-4DA8-82C5-2E81DAB56F23"; // US-1 Demo
+    private static final String APP_ID = "91EEEE6D-8D4A-4FA4-9A52-0B8E2DDD64D0"; // SouthKernTest01
     public static final String VERSION = "3.0.36";
 
     @Override
